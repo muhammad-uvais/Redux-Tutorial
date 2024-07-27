ae49a1aa17cae8fe2ec212e16fc3c65de57df6f6
mohammad uvais <mohammaduvais517@gmail.com>
2024-07-27 16:52:51 +0530

    removed uneccesary imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import Navbar from './Components/Navbar';
 import Main from './Components/Main';
 
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,4 +1,4 @@
-import React , {useState} from 'react'
+import {useState} from 'react'
 import { useDispatch } from 'react-redux';
 import { addToCart, decreaseFomCart, emptyCart } from '../Redux/action';
 import { useSelector } from 'react-redux';
